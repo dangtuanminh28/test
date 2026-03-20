f4fd2dcf2306fe979d9570f2453cd7524b772240
Đặng Tuấn Minh <dangminh28122006@gmail.com>
2026-03-20 11:05:08 +0700

    Seok

---
+++ ok/src/No.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class No {
+    public static void main(String[] args) {
+        System.out.println("Ok");
+        Scanner sc = new Scanner(System.in);
+        int ok = sc.nextInt();
+    }
+}
+//ok
+//ok
+//odokđk
